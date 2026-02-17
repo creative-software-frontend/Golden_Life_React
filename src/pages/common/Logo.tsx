4d30d9f0fe808ab0bf8e183125d182114561260c
rimi-124 <zfarhana156@gmail.com>
2026-02-17 14:23:19 +0600

    logo add in landing page

--- src/pages/common/Logo.tsx
+++ src/pages/common/Logo.tsx
@@ -1,14 +1,12 @@
 
-
 export default function Logo() {
   return (
-    <div className="">
-      <div className="font-sans font-bold text-black tracking-wide">
-        <span className="text-2xl md:text-3xl lg:text-4xl">GOLDEN</span>
-      </div>
-      <div className="font-sans font-bold text-[#FF8A00] tracking-wide mt-[-4px]">
-        <span className="text-2xl md:text-3xl lg:text-4xl">LIFE</span>
-      </div>
+    <div className="flex items-center">
+      <img 
+        src="/image/logo/logo.jpg" 
+        alt="Golden Life Logo" 
+        className="h-12 w-auto object-contain" 
+      />
     </div>
   );
 }
